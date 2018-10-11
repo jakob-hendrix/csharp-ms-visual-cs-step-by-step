a4f3ff6aae5b4fc91e776c6700f329474b6abc6f
jakob-hendrix <jhendrix001@gmail.com>
2018-10-11 15:54:03 -0500

    Current state

---
+++ TestHello/TestHello/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jakob";
+            int negative = -5 / 2;
+            int positive = 
+
+            Console.WriteLine($"Hello good sir, {name}!");
+        }
+    }
+}
---
+++ samples/Chapter 3/DailyRate/DailyRate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double v) => Console.WriteLine($"The consultant's fee is: ${v * 1.1}");
+
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+        private int readInt(string v) => int.Parse(answerPrompt(v));
+    
+        private double readDouble(string v) => double.Parse(answerPrompt(v));
+
+        private static string answerPrompt(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+    }
+}
