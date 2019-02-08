703bbf03572a1b887e082468352dcc7fb5c9b475
p00293412 <jakob.hendrix@la.gov>
2019-02-08 16:21:14 -0600

    Some changes

--- PracticeApp/PracticeApp/PracticeApp/Program.cs
+++ PracticeApp/PracticeApp/PracticeApp/Program.cs
@@ -10,24 +10,31 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            int[] ReadData()
-            {
-                Console.WriteLine("How many elements");
-                string reply = Console.ReadLine();
-                int numElements = int.Parse(reply);
+            Person jakob = new Person("Jakob Hendrix", 30);
+            Console.WriteLine(jakob);
 
-                int[] data = new int[numElements];
-                for (int i = 0; i < numElements; i++)
-                {
-                    Console.WriteLine($"Enter data for element {i}");
-                    reply = Console.ReadLine();
-                    int elementData = int.Parse(reply);
-                    data[i] = elementData;
-                }
-                return data;
+            Object somebody = jakob;
+            Console.WriteLine(somebody);
+
+            jakob = (Person)somebody;
+
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
             }
 
-            int[] userData = ReadData();
+            public override string ToString()
+            {
+                return $"This is a person named {this.Name} whom is {this.Age} years old.";
+            }
         }
     }
 }
